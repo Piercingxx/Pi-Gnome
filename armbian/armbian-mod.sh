4b5101d10b3cf737a6156ce44db2014f04270bbc
Piercingxx <155384428+Piercingxx@users.noreply.github.com>
2025-11-11 20:41:53 -0500

    Update armbian-mod.sh

--- armbian/armbian-mod.sh
+++ armbian/armbian-mod.sh
@@ -55,10 +55,8 @@ function msg_box() {
 function menu() {
     whiptail --backtitle "GitHub.com/PiercingXX" --title "Main Menu" \
         --menu "Run Options In Order:" 0 0 0 \
-        "Install"                               "Install PiercingXX Debian" \
-        "Nvidia Driver"                         "Install Nvidia Drivers (Do not install on a Surface Device)" \
-        "Optional Surface Kernel"               "Microsoft Surface Kernel" \
-        "Hyprland"                              "**Currently Broken** Install Hyprland & All Dependencies" \
+        "Install Support and Drivers"           "Install PiercingXX Armbian-Mod" \
+        "Install Gnome"                         "Install Gnome and Apps onArmbian" \
         "Reboot System"                         "Reboot the system" \
         "Exit"                                  "Exit the script" 3>&1 1>&2 2>&3
 }
@@ -68,16 +66,37 @@ while true; do
     echo -e "${GREEN}Welcome ${username}${NC}\n"
     choice=$(menu)
     case $choice in
-        "Install")
+        "Install Support and Drivers")
             echo -e "${YELLOW}Updating System...${NC}"
-            # Turn off sleep/suspend to avoid interruptions
-                gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'false'
-                gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'false'
-                gsettings set org.gnome.settings-daemon.plugins.power idle-dim 'false'
-            # Install Gnome and Dependencies
+            # Install Dependencies
                 cd scripts || exit
                 chmod u+x step-1.sh
                 sudo ./step-1.sh
+                wait
+                cd "$builddir" || exit
+            # Apply Piercing Rice
+                echo -e "${YELLOW}Applying PiercingXX Gnome Customizations...${NC}"
+                rm -rf piercing-dots
+                git clone --depth 1 https://github.com/Piercingxx/piercing-dots.git
+                cd piercing-dots || exit
+            # Replace .bashrc
+                cp -f piercing-dots/resources/bash/.bashrc /home/"$username"/.bashrc
+                source ~/.bashrc
+            # Bash Stuff
+                install_bashrc_support
+            # Copy over scripts
+
+            # Clean Up
+                rm -rf piercing-dots
+            echo -e "${GREEN}PiercingXX Gnome Customizations Applied successfully!${NC}"
+            msg_box "System will reboot now."
+            sudo reboot
+            ;;
+        "Install Gnome")
+            # Install Gnome and Dependencies
+                cd scripts || exit
+                chmod u+x gnome-and-apps.sh
+                sudo ./gnome-and-apps.sh
                 wait
                 cd "$builddir" || exit
             # Apply Piercing Rice
@@ -89,55 +108,11 @@ while true; do
                 ./install.sh
                 wait
                 cd "$builddir" || exit
-            # Install Apps & Dependencies
-                echo -e "${YELLOW}Installing Apps & Dependencies...${NC}"
-                cd scripts || exit
-                chmod u+x apps.sh
-                sudo ./apps.sh
-                wait
-                cd "$builddir" || exit
             # Apply Piercing Gnome Customizations as User
                 cd piercing-dots/scripts || exit
                 ./gnome-customizations.sh
                 wait
                 cd "$builddir" || exit
-            # Replace .bashrc
-                cp -f piercing-dots/resources/bash/.bashrc /home/"$username"/.bashrc
-                source ~/.bashrc
-            # Bash Stuff
-                install_bashrc_support
-            # Clean Up
-                rm -rf piercing-dots
-            echo -e "${GREEN}PiercingXX Gnome Customizations Applied successfully!${NC}"
-            msg_box "System will reboot now."
-            sudo reboot
-            ;;
-        "Nvidia Driver")
-            echo -e "${YELLOW}Installing Nvidia Drivers...${NC}"
-            # Install Nvidia Drivers
-                cd scripts || exit
-                chmod u+x nvidia.sh
-                sudo ./nvidia.sh
-                wait
-                cd "$builddir" || exit
-            echo -e "${GREEN}Nvidia Drivers Installed Successfully!${NC}"
-            msg_box "Nvidia Drivers installed successfully. Reboot the system to apply changes."
-            sudo reboot
-            ;;
-        "Optional Surface Kernel")
-            echo -e "${YELLOW}Microsoft Surface Kernel...${NC}"            
-                cd scripts || exit
-                chmod u+x Surface.sh
-                sudo ./Surface.sh
-                cd "$builddir" || exit
-            ;;
-        "Hyprland"*)
-            echo -e "${YELLOW}Installing Hyprland & Dependencies...${NC}"
-                cd scripts || exit
-                chmod u+x hyprland-install.sh
-                ./hyprland-install.sh
-                cd "$builddir" || exit
-            echo -e "${GREEN}Installed successfully!${NC}"
             ;;
         "Reboot System")
             echo -e "${YELLOW}Rebooting system in 3 seconds...${NC}"
